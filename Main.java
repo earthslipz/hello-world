fd5bc3ed0b24309a333a15f5e905daab19907771
Phubase San <130052369+earthslipz@users.noreply.github.com>
2025-04-11 13:29:15 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class asd1234{
+public static void main(String[] args){
+int[] arr={10,5,3,7,1};
+sortMyStuff(arr);
+for(int i=0;i<arr.length;i++){
+System.out.println(arr[i]);}
+}
+public static void sortMyStuff(int[] a){
+for(int i=0;i<a.length;i++){ 
+for(int j=i+1;j<a.length;j++){
+if(a[i]>a[j]){
+int temp=a[i];
+a[i]=a[j];
+a[j]=temp;}
+}
+}
+}
+}
